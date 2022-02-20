8116d3f4daf3d4429b5ba43a6d82d4e46aa58761
Jordi <kozko2001@gmail.com>
2022-02-20 09:28:34 +0000

    add parent variable to CI

--- components/VideoEmbed.tsx
+++ components/VideoEmbed.tsx
@@ -4,7 +4,8 @@ export declare interface VideoEmbedProps {
 
 
 const VideoEmbed = (props: VideoEmbedProps) => {
-    const url = `https://player.twitch.tv/?video=${props.video_id}&parent=localhost&autoplay=true&muted=false`
+    const parent = process.env.NEXT_PUBLIC_TWITCH_PARENT
+    const url = `https://player.twitch.tv/?video=${props.video_id}&parent=${parent}&autoplay=true&muted=false`
     return (
         <div className='sm:col-span-1 md:col-span-3 lg:col-span-4 bg-indigo-400 row-span-3'>
         <iframe
@@ -12,7 +13,7 @@ const VideoEmbed = (props: VideoEmbedProps) => {
           className='aspect-video'
           src={url}
           width="100%"
-          allowFullscreen="true">
+          allowFullScreen>
       </iframe>
       </div>
     )
